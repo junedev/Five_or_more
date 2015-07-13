4aa16aa71b8664f02e55970ea593a0a61c2d669f
fran-k <franziskaklingner@hotmail.com>
2015-07-13 15:04:41 +0100

    Rank is displayed after game is finished

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var Fom = Fom || {};
 Fom.setup = function(){
 	this.size = 9;
 	this.neighbourMap = [];
-	this.neighbourMapInclDiagonal = null;
+	this.neighbourMapInclDiagonal = [];
 	this.emptyArea = null;
 	this.checkedIds = null;
 	this.score = 0;
 	this.scoreMap = {};
+	this.rankMap = [[0, "LOOSER"],[200,"BRONZE"],[500, "SILVER"],[1000, "GOLD"],[2000,"LEGEN-DARY"]];
 	Fom.fillScoreMap();
 	Fom.createGrid();
 	Fom.addThreeBubbles();
@@ -41,7 +42,6 @@ Fom.createGrid = function (){
 		newBox.disabled=true;
 	}
 	Fom.fillNeighbourMap();
-	this.neighbourMapInclDiagonal=this.neighbourMap;
 	Fom.fillNeighbourMapInclDiagonal();
 }
 
@@ -58,6 +58,15 @@ Fom.fillNeighbourMap = function(){
 
 Fom.fillNeighbourMapInclDiagonal = function(){
 	for(var i=0; i<(Fom.size*Fom.size);i++){
+		var neighbours=[];		
+		if(i-Fom.size>=0){neighbours.push(i-Fom.size);} else{neighbours.push(null);} //top
+		if(i+Fom.size<(Fom.size*Fom.size)){neighbours.push(i+Fom.size);} else{neighbours.push(null);} //bottom
+		if(i%Fom.size!==0){neighbours.push(i-1);} else{neighbours.push(null);} //left
+		if(i%Fom.size!==Fom.size-1){neighbours.push(i+1);} else{neighbours.push(null);} //right
+		Fom.neighbourMapInclDiagonal.push(neighbours);
+	}
+
+	for(var i=0; i<(Fom.size*Fom.size);i++){
 		if(i-Fom.size-1>=0 && i%Fom.size!==0){Fom.neighbourMapInclDiagonal[i].push(i-Fom.size-1)} //top left
 			else{Fom.neighbourMapInclDiagonal[i].push(null);};
 		if(i+Fom.size+1<(Fom.size*Fom.size) && i%Fom.size!==Fom.size-1){Fom.neighbourMapInclDiagonal[i].push(i+Fom.size+1)} //bottom right
@@ -72,6 +81,12 @@ Fom.fillNeighbourMapInclDiagonal = function(){
 Fom.isFull=function(){
 	var $emptyBoxes = $(".box[state='empty']");
 	if($emptyBoxes.length===0){
+		for(var i=Fom.rankMap.length-1;i>=0;i--){
+			if(Fom.score>=Fom.rankMap[i][0]){
+				alert("You earned the rank "+Fom.rankMap[i][1]+"!");
+				i=-1;
+			}
+		}
 		return true;
 	} else {
 		return false;
@@ -81,12 +96,13 @@ Fom.isFull=function(){
 Fom.addThreeBubbles=function(){
 	for(var i=0;i<3;i++){
 		var $emptyBoxes = $(".box[state='empty']");
-		if(!Fom.isFull()){
+		if($emptyBoxes.length>0){
 			var box = $emptyBoxes[Math.floor(Math.random()*$emptyBoxes.length)];
 			Fom.addBubble(box);
 			$(box).attr("state","taken");
 			Fom.checkRemoval(box);
-		}
+			Fom.isFull();
+		} 
 	}
 }
 
@@ -113,7 +129,7 @@ Fom.bubbleEvent = function(){
 }
 
 Fom.boxEvent = function(){
-	//console.log("box clicked");
+	$("#message").html("");
 	if(Fom.validMove(event.currentTarget,$(".selected")[0])){
 		$(event.currentTarget).attr("state","taken");
 		$(".selected").parent().attr("state","empty");
@@ -123,79 +139,79 @@ Fom.boxEvent = function(){
 		Fom.addThreeBubbles();
 		$(".box").prop("disabled",true);}
 		else{
-		//show "you can't move there" on display
+			$("#message").html("You can't move there.")
+		}
 	}
-}
 
-Fom.validMove = function(targetBox,bubble){
-	var targetId = parseInt(targetBox.id);
-	var bubbleId = parseInt($(bubble).parent()[0].id);
-	var result = false;
-	Fom.emptyArea = [];
-	Fom.checkedIds = [];
+	Fom.validMove = function(targetBox,bubble){
+		var targetId = parseInt(targetBox.id);
+		var bubbleId = parseInt($(bubble).parent()[0].id);
+		var result = false;
+		Fom.emptyArea = [targetId];
+		Fom.checkedIds = [];
 
-	var checkForEmptyNeighbours = function(id){
-		if($.inArray(id,Fom.checkedIds)===-1){
-			Fom.checkedIds.push(id);
-			for(var i=0;i<Fom.neighbourMap[id].length;i++){
-				nId = Fom.neighbourMap[id][i];
-				if($("#"+nId).attr("state")==="empty"){
-					if($.inArray(nId,Fom.emptyArea)===-1){
-						Fom.emptyArea.push(nId);
-						checkForEmptyNeighbours(nId);
+		var checkForEmptyNeighbours = function(id){
+			if(!isNaN(id) && $.inArray(id,Fom.checkedIds)===-1){
+				Fom.checkedIds.push(id);
+				for(var i=0;i<Fom.neighbourMap[id].length;i++){
+					nId = Fom.neighbourMap[id][i];
+					if($("#"+nId).attr("state")==="empty"){
+						if($.inArray(nId,Fom.emptyArea)===-1){
+							Fom.emptyArea.push(nId);
+							checkForEmptyNeighbours(nId);
+						}
 					}
 				}
 			}
 		}
-	}
 
-	checkForEmptyNeighbours(targetId);
-	for(var i=0;i<Fom.neighbourMap[bubbleId].length;i++){
-		if($.inArray(Fom.neighbourMap[bubbleId][i],Fom.emptyArea)!==-1){
-			result=true;
-		}
-	}
-	return result; 
-}
-
-Fom.checkRemoval = function (startBox){
-	Fom.color = $(startBox).children().css("background-color");
-	Fom.counter = [1,1,1,1];
-	var startId = parseInt(startBox.id);
-	var startN = Fom.neighbourMapInclDiagonal[startId];
-	var storage = [[],[],[],[]];
-	var bubbleCount = 0;
-	
-	var checkColor = function(index, id, neighbour){
-		if(!isNaN(neighbour)){
-			if( $("#"+neighbour).children().length>0 && $("#"+neighbour).children().css("background-color")===Fom.color){
-				Fom.counter[index]++;
-				storage[index].push($("#"+neighbour));
-				checkColor(index,neighbour, neighbour+(neighbour-id));
-			} else {
-				return
+		checkForEmptyNeighbours(targetId);
+		for(var i=0;i<Fom.neighbourMap[bubbleId].length;i++){
+			if($.inArray(Fom.neighbourMap[bubbleId][i],Fom.emptyArea)!==-1){
+				result=true;
 			}
 		}
+		return result; 
 	}
 
-	for(var i=0; i<Fom.counter.length;i++){
-		checkColor(i,startId,startN[i*2]);
-		checkColor(i,startId,startN[i*2+1]);
-	}
+	Fom.checkRemoval = function (startBox){
+		Fom.color = $(startBox).children().css("background-color");
+		Fom.counter = [1,1,1,1];
+		var startId = parseInt(startBox.id);
+		var startN = Fom.neighbourMapInclDiagonal[startId];
+		var storage = [[],[],[],[]];
+		var bubbleCount = 0;
 
-	for(var i=0; i<Fom.counter.length;i++){
-		if(Fom.counter[i]>=5){
-			bubbleCount+=Fom.counter[i]-1;
-			$(startBox).empty();
-			$(startBox).attr("state","empty");
-			$(storage[i]).each(function(index,element){
-				$(element).empty();
-				$(element).attr("state","empty");
-			});
+		var checkColor = function(index, id, neighbour){
+			if(!isNaN(neighbour)){
+				if( $("#"+neighbour).children().length>0 && $("#"+neighbour).children().css("background-color")===Fom.color){
+					Fom.counter[index]++;
+					storage[index].push($("#"+neighbour));
+					checkColor(index,neighbour, neighbour+(neighbour-id));
+				} else {
+					return
+				}
+			}
 		}
-	}
 
-	if(bubbleCount!==0){
+		for(var i=0; i<Fom.counter.length;i++){
+			checkColor(i,startId,startN[i*2]);
+			checkColor(i,startId,startN[i*2+1]);
+		}
+
+		for(var i=0; i<Fom.counter.length;i++){
+			if(Fom.counter[i]>=5){
+				bubbleCount+=Fom.counter[i]-1;
+				$(startBox).empty();
+				$(startBox).attr("state","empty");
+				$(storage[i]).each(function(index,element){
+					$(element).empty();
+					$(element).attr("state","empty");
+				});
+			}
+		}
+
+		if(bubbleCount!==0){
 		bubbleCount++; //add start bubble to connected bubbles count
 		Fom.score+=Fom.scoreMap[bubbleCount];
 		$("#score").html("Score: "+Fom.score);
