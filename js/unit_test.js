5d48936ef73fd6975ac91e7f63810dd7e71730d2
junedev <catherinejune86@gmail.com>
2015-09-27 17:06:41 +0200

    simplified syntax for unit test for game controller

--- js/unit_test.js
+++ js/unit_test.js
@@ -8,30 +8,29 @@ describe("GameController", function() {
   }));
 
   describe("setup field", function() {
-    var scope, controller;
+    var controller;
     beforeEach(function() {
-      scope = {};
-      controller = $controller('GameController as game', { $scope: scope });
+      game = $controller('GameController');
     });
 
     it("has 81 boxes", function() {
-      expect(scope.game.boxes.length).to.equal(81);
+      expect(game.boxes.length).to.equal(81);
     });
 
     it("has 3 bubbles in preview", function(){
-      expect(scope.game.preview.length).to.equal(3);
+      expect(game.preview.length).to.equal(3);
     });
 
     it("has 3 bubbles on the field", function(){
       var counter = 0;
-      for(var i=0; i<scope.game.boxes.length; i++){
-        if(scope.game.boxes[i]) counter++;
+      for(var i=0; i<game.boxes.length; i++){
+        if(game.boxes[i]) counter++;
       }
       expect(counter).to.equal(3);
     });
 
     it("has no bubble selected", function(){
-      expect(scope.game.activeBubble).to.not.exist;
+      expect(game.activeBubble).to.not.exist;
     })
   });
 
